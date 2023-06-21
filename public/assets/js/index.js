584a8662b07ebec94aa57425dfddbefdb774c73d
Jorge Duarte <sadujorge@gmail.com>
2023-06-20 22:27:28 -0700

    Deleted code relating to debugging

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -73,7 +73,6 @@ const handleNoteSave = () => {
     text: noteText.value,
   };
   saveNote(newNote).then(() => {
-    console.log(newNote);
     getAndRenderNotes();
     renderActiveNote();
   });
